b85cc3ff8463dc7cc71f01c2c34718d9a922bc45
yaoji <1907836146@qq.com>
2022-03-01 01:34:08 +0800

    feat(server): HyBase客户端封装 HyBase客户端封装调用/解析，日志组件引入

---
+++ client/hybase_client.go
@@ -0,0 +1,85 @@
+// Package client
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"hybase_exporter/common/log"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+)
+
+const (
+	//hybasePublicStatus status url
+	hybasePublicStatus = "/public/status.do"
+)
+
+// HyBaseResult HyBase Api基础返回实体
+type HyBaseResult struct {
+	Code    int          `json:"code"`
+	Message string       `json:"message"`
+	Data    []HyBaseNode `json:"data"`
+}
+
+//HyBaseItem 各个指标数据
+type HyBaseItem struct {
+	Unit  string `json:"unit"`
+	Name  string `json:"name"`
+	Value string `json:"value"`
+	Key   string `json:"key"`
+}
+type HyBaseNode struct {
+	Id       string       `json:"id"`
+	Ip       string       `json:"ip"`
+	Sysname  string       `json:"sysname"`
+	Itemlist []HyBaseItem `json:"itemlist"`
+}
+
+//HyBaseClient Get the hybase http client instance
+type HyBaseClient struct {
+	protocol string
+	ip       string
+	port     int
+}
+
+//getHyBaseUrl Get the api address based on the underlying path and URI
+func getHyBaseUrl(client HyBaseClient, uri string) string {
+	return client.protocol + "://" + client.ip + ":" + strconv.Itoa(client.port) + uri
+}
+
+//GetPublicStatus Send http request to hybase server and parse res to struct HyBaseResult
+func GetPublicStatus(client HyBaseClient) HyBaseResult {
+	url := getHyBaseUrl(client, hybasePublicStatus)
+	log.Info.Log("msg", "send request to "+url)
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Error.Log("msg", err)
+		return HyBaseResult{}
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Error.Log("msg", err)
+		return HyBaseResult{}
+	}
+	return parseHyBaseResult(body)
+}
+
+//parseHyBaseResult parse hybase api result json bytes to struct
+func parseHyBaseResult(result []byte) HyBaseResult {
+	var hbr HyBaseResult
+	err := json.Unmarshal(result, &hbr)
+	if err != nil {
+		log.Warn.Log("msg", "parse hybase json result error", "result", string(result), "err", err)
+		return HyBaseResult{}
+	}
+	return hbr
+}
+
+func main() {
+	client := HyBaseClient{"http", "127.0.0.1", 5555}
+	fmt.Println(client)
+	status := GetPublicStatus(client)
+	fmt.Println(status)
+}
---
+++ common/log/logger_init.go
@@ -0,0 +1,30 @@
+package log
+
+import (
+	"fmt"
+	"github.com/go-kit/log"
+	"github.com/go-kit/log/level"
+	"github.com/prometheus/common/promlog"
+	"github.com/prometheus/common/promlog/flag"
+	"gopkg.in/alecthomas/kingpin.v2"
+	"os"
+)
+
+var Debug log.Logger
+var Info log.Logger
+var Warn log.Logger
+var Error log.Logger
+
+func init() {
+	promLogConfig := &promlog.Config{}
+	logger := promlog.New(promLogConfig)
+	flag.AddFlags(kingpin.CommandLine, promLogConfig)
+	kingpin.CommandLine.UsageWriter(os.Stdout)
+	kingpin.HelpFlag.Short('h')
+	kingpin.Parse()
+	Debug = level.Debug(logger)
+	Info = level.Info(logger)
+	Warn = level.Warn(logger)
+	Error = level.Error(logger)
+	fmt.Println("init logger debug|info|warn|error ...")
+}
